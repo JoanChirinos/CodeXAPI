72b1f9962251a531ee573e6ad179cbe64e653d8b
Joan Chirinos <jchirinos3201@gmail.com>
2019-05-24 18:13:58 +0000

    beans

--- util/run.py
+++ util/run.py
@@ -16,16 +16,16 @@ def go(user_code, real_filename):
     filename = str(uuid.uuid4())
     with open('/home/joan/code/{}.py'.format(filename), 'w') as f:
         f.write(user_code)
-    my_stdout = open('{}_stdout.txt', 'w')
-    my_stderr = open('{}_stderr.txt', 'w')
+    my_stdout = open('{}_stdout.txt'.format(filename), 'w')
+    my_stderr = open('{}_stderr.txt'.format(filename), 'w')
     user_p = subprocess.run(['/usr/bin/python3', '/home/joan/code/{}.py'.format(filename)], stdout=my_stdout, stderr=my_stderr, timeout=10)
     my_stdout.close()
     my_stderr.close()
-    with open('{}_stdout.txt') as f:
+    with open('{}_stdout.txt'.format(filename)) as f:
         my_stdout = f.read()
-    with open('{}_stderr.txt') as f:
+    with open('{}_stderr.txt'.format(filename)) as f:
         my_stderr = f.read()
-    
+
     return str(my_stdout), str(my_stderr).replace('{}.py'.format(filename), real_filename)
 
 ##out, err = go('print("works!")', 'testcode.py')
