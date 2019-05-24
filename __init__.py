eaf539a2b0f88b9f48a6d93c67ed59cbf318c38f
= <=>
2019-05-24 13:42:21 -0400

    added stuff

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 
-from flask import Flask
+from flask import Flask, jsonify
+
+from XAPI.util import run
 
 app = Flask(__name__)
 app.secret_key = 'beans'
@@ -8,8 +10,24 @@ app.secret_key = 'beans'
 
 @app.route('/')
 def home():
-    return 'Hello!'
+    return "There's nothing here!"
 
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+
+@app.route('/run', methods=["POST"])
+def run_file():
+    code = str(request.form['code'])
+    filename = str(request.form['filename'])
+
+    stdout, stderr = run.go(code, filename)
+
+    return jsonify(stdout=stdout,
+                   stderr=stderr)
+    
 
 if __name__ == '__main__':
     app.debug = True
