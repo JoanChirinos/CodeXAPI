08a9f547ffad2b4bf191764bdb9ab2b5e4d9d1a9
= <=>
2019-05-28 12:26:27 -0400

    pls:

--- util/run.py
+++ util/run.py
@@ -14,16 +14,16 @@ def go(user_code, real_filename):
     if unsafe:
         return 'Unsafe code! I can\'t run that!'
     filename = str(uuid.uuid4())
-    with open('/home/joan/code/{}.py'.format(filename), 'w') as f:
+    with open('/var/www/XAPI/XAPI/util/{}.py'.format(filename), 'w') as f:
         f.write(user_code)
-    my_stdout = open('{}_stdout.txt', 'w')
-    my_stderr = open('{}_stderr.txt', 'w')
-    user_p = subprocess.run(['/usr/bin/python3', '/home/joan/code/{}.py'.format(filename)], stdout=my_stdout, stderr=my_stderr, timeout=10)
+    my_stdout = open('/var/www/XAPI/XAPI/util/{}_stdout.txt', 'w')
+    my_stderr = open('/var/www/XAPI/XAPI/util/{}_stderr.txt', 'w')
+    user_p = subprocess.run(['/usr/bin/python3', '/var/www/XAPI/XAPI/util/{}.py'.format(filename)], stdout=my_stdout, stderr=my_stderr, timeout=10)
     my_stdout.close()
     my_stderr.close()
-    with open('{}_stdout.txt') as f:
+    with open('/var/www/XAPI/XAPI/util/{}_stdout.txt') as f:
         my_stdout = f.read()
-    with open('{}_stderr.txt') as f:
+    with open('/var/www/XAPI/XAPI/util/{}_stderr.txt') as f:
         my_stderr = f.read()
     
     return str(my_stdout), str(my_stderr).replace('{}.py'.format(filename), real_filename)
